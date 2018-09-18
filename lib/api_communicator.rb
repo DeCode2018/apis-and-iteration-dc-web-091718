0c5a909ee6cc280ee1b3f78851f597a3e8189be2
DeCode2018 <decode2018@gmail.com>
2018-09-18 16:29:53 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,16 +2,34 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+
 def get_character_movies_from_api(character)
   #make the web request
+  film_list = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # binding.pry
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
   # iterate over the response hash to find the collection of `films` for the given
+  response_hash["results"].each do |item|
+    if item["name"].downcase == character
+      film_list = item["films"]
+    end
+
+  end
+
+  movie_list = film_list.map do |film|
+    response_string = RestClient.get(film)
+  # binding.pry
+    response_hash = JSON.parse(response_string)
+  end
+
+
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
@@ -22,8 +40,20 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def print_movies(movie_list)
+
+  movie_list.each do |item|
+    puts item['title']
+  end
+  # # some iteration magic and puts out the movies in a nice list
+    #   film_list.each do |film|
+    #   response_string = RestClient.get(film)
+    #   # binding.pry
+    #   response_hash = JSON.parse(response_string)
+    #   puts response_hash['title']
+    #   binding.pry
+    # end
+
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars API"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  puts "************************"
   # use gets to capture the user's input. This method should return that input, downcased.
+  return character = gets.chomp.downcase
+  puts "************************\n\n"
 end
